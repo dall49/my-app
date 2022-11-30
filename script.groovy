a807122716933c78e30c8518974f9ddda67e64b5
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-11-30 16:53:30 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,8 @@ def buildImg() {
      sh 'docker build -t youssefjeh/my-app:3.0 .'
     withCredential([usernamePassword(credentialsId:'dockerhub-rep', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t youssefjeh/my-app:3.0 .'
-        sh 'docker tag dockerysf/my-app:3.0 youssefjeh/my-app:3.0'
+       // sh 'docker tag dockerysf/my-app:3.0 youssefjeh/my-app:3.0'
+        sh 'docker tag youssefjeh/my-app:3.0 dockerysf/my-app:3.0'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push dockerysf/my-app:3.0'
      }
