de2659566769c7dec36b878a64600f1956f53ad6
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-12-04 18:16:17 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,24 +1,3 @@
-def buildJar() {
-     echo 'building Jar file...'
-     sh 'mvn package'
-}
-
-def buildImg() {
-     echo 'building docker image..'
-     //sh 'docker build -t youssefjeh/my-app:3.0 .'
-    withCredentials([usernamePassword(credentialsId:'dockerhub-rep', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t youssefjeh/my-app:3.0 .'
-       // sh 'docker tag dockerysf/my-app:3.0 youssefjeh/my-app:3.0'
-        sh 'docker tag youssefjeh/my-app:3.0 dockerysf/my-app:3.0'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push dockerysf/my-app:3.0'
-     }
-}
-
-/*def testApp() {
-     echo 'testing app...'
-}*/
-
 def deployApp() {
      echo 'deploying app...'
      // echo "deploying version ${params.VERSION}"
